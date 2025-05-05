c42180772a1433a07bb27859cd03c224ca1eb1d8
siqi.pan <siqi.pan.7e7@itb.cat>
2025-05-05 10:43:28 +0200

    exercice 2 advance

--- cat.itb.store_VillodresAdrian/empDAO/EmployeeDAO.cs
+++ cat.itb.store_VillodresAdrian/empDAO/EmployeeDAO.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using cat.itb.gestioHR.depDAO;
 
 namespace cat.itb.store_VillodresAdrian.empDAO
 {
-    public class EmployeeDAO
+    public interface EmployeeDAO
     {
+        void DeleteAll();
+        void InsertAll(List<Employee> emps);
+        List<Employee> SelectAll();
+        Employee Select(int empId);
+        Boolean Insert(Employee emp);
+
+        Boolean Delete(int empId);
+
+        Boolean Update(Employee emp);
     }
 }
